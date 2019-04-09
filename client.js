c96b0a4d45084fa9d13349de1264ffa7e425ab6e
Meyer <ehlmeyer@gmail.com>
2019-04-09 16:50:00 -0500

    working on getting the return to show the whole object

--- client.js
+++ client.js
@@ -17,7 +17,7 @@ const employees = [
   {
     name: 'Scout',
     employeeNumber: '6243',
-    annualSalary: '74750',
+    annualSalary: '74000',
     reviewRating: 5
   },
   {
@@ -37,41 +37,58 @@ const employees = [
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
 //loop through employee array
-let employeeBonusInfo = {
-  name:'',
-  bonusPercentage: 0,
-  totalCompensation: 0,
-  totalBonus: 0
-}; //creating new employee object
-let employee = {
-  name: '',
-  employeeNumber: '',
-  annualSalary: '',
-  reviewRating: 0
-};
+//creating new employee object
+// let employee = {
+//   name: '',
+//   employeeNumber: '',
+//   annualSalary: '',
+//   reviewRating: 0
+// };
 //let bonusPercentage = 0;
 
 
-  function bonusCalculation(employee) {
-    for (i = 0; i < employees.length; i++) {
-      employee = employees[i];
-      console.log(employees[i]);
-    if (employee.reviewRating <= 2) {      
+function bonusCalculation(employee) {
+  let employeeBonusInfo = {
+    name: '',
+    bonusPercentage: 0,
+    totalCompensation: 0,
+    totalBonus: 0
+  };
+function findBonusPercentage(){
+  for (i = 0; i < employees.length; i++) {
+    employee = employees[i];
+    console.log(employees[i]);
+    if (employee.reviewRating <= 2) {
       employeeBonusInfo.bonusPercentage = 0;
       console.log(employeeBonusInfo);
     } else if (employees[i].reviewRating === 3) {
       employeeBonusInfo.bonusPercentage = 4;
       console.log(employeeBonusInfo);
     } else if (employees[i].reviewRating === 4) {
-      employeeBonusInfo.bonusPercentage = 6; 
+      employeeBonusInfo.bonusPercentage = 6;
       console.log(employeeBonusInfo);
-  } else if (employees[i].reviewRating === 5) {
-    employeeBonusInfo.bonusPercentage = 10;
+    } else if (employees[i].reviewRating === 5) {
+      employeeBonusInfo.bonusPercentage = 10;
       console.log(employeeBonusInfo);
-  }
+    }
+    if (employees[i].employeeNumber.length === 4) {
+      employeeBonusInfo.bonusPercentage += 5;
+      console.log(employeeBonusInfo);
+    }
+    if (employees[i].annualSalary > 65000 && employees[i].reviewRating > 2) {
+      employeeBonusInfo.bonusPercentage = 1;
+      console.log(employeeBonusInfo);
+    }
+    if (employeeBonusInfo.bonusPercentage < 0) {
+      employeeBonusInfo.bonusPercentage = 0;
+      console.log(employeeBonusInfo);
+    } else if (employeeBonusInfo.bonusPercentage > 13) {
+      employeeBonusInfo.bonusPercentage = 13;
+      console.log(employeeBonusInfo);
+    }
   }//end loop
-  
-  
+  return employeeBonusInfo.bonusPercentage;
+}
 }
 
 // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
@@ -81,4 +98,4 @@ let employee = {
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
